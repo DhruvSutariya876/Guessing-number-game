6d72657b095b833b228eba9241c4d3c8314100e9
DhruvSutariya876 <dhruvsutariya222@gmail.com>
2025-08-17 13:40:05 +0530

    Update guess.js

--- guess.js
+++ guess.js
@@ -1,16 +1,37 @@
 let homediv = document.getElementById('homepage');
 let guessdiv = document.getElementById('guesspage');
 let greet = document.getElementById('wlctext');
-document.getElementById('btn').addEventListener('click',()=>{
+let no_guess=0;
+let random_no;
+document.getElementById('btn').addEventListener('click', () => {
     const start = parseInt(document.getElementById('start').value);
     const end = parseInt(document.getElementById('end').value);
-    if(isNaN(start)||isNaN(end) || (start>=end)){
+    if (isNaN(start) || isNaN(end) || (start >= end)) {
         alert("Enter valid range!!!");
         return;
     }
-    let random_no = Math.floor(Math.random()*(end-start)+start);
-    
-    homediv.style.display="none";
-    guessdiv.style.display="block";
-    greet.textContent="Guess the number";
+    random_no = Math.floor(Math.random() * (end - start+1) + start);
+
+    homediv.style.display = "none";
+    guessdiv.style.display = "block";
+    greet.textContent = "Guess the number";
 })
+document.getElementById('guessed').addEventListener('click', checkGuessNo);
+document.getElementById('restart').addEventListener('click', () => {
+    location.reload();
+});
+function checkGuessNo() {
+    const user_guess = parseInt(document.getElementById('userguess').value);
+    let result = document.getElementById('result');
+    no_guess++;
+    if (user_guess === random_no) {
+        result.textContent = "🎉 Correct! You guessed the number! in " + no_guess + " attempts";
+        document.getElementById('userguess').style.display="none";
+    } else if (user_guess > random_no) {
+        result.textContent = "📉 Too high! Try again.";
+    } else {
+        result.textContent = "📉 Too Low! Try again.";
+    }
+
+
+}
